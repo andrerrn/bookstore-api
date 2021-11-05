7685ea05241ad7951381f660528cce49bd2fa746
andrerrn <andre.paz.gomes@gmail.com>
2021-11-04 22:33:55 -0300

    Classes de domínio iniciaadas

---
+++ src/main/java/com/andre/bookstore/domain/Livro.java
@@ -0,0 +1,97 @@
+package com.andre.bookstore.domain;
+
+import java.util.Objects;
+
+public class Livro {
+	private Integer id;
+	private String titulo;
+	private String nome_autor;
+	private String texto;
+	
+	
+	private Categoria categoria;
+
+
+	public Livro(Integer id, String titulo, String nome_autor, String texto, Categoria categoria) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.nome_autor = nome_autor;
+		this.texto = texto;
+		this.categoria = categoria;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+	public String getNome_autor() {
+		return nome_autor;
+	}
+
+
+	public void setNome_autor(String nome_autor) {
+		this.nome_autor = nome_autor;
+	}
+
+
+	public String getTexto() {
+		return texto;
+	}
+
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(categoria, id, nome_autor, texto, titulo);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		return Objects.equals(categoria, other.categoria) && Objects.equals(id, other.id)
+				&& Objects.equals(nome_autor, other.nome_autor) && Objects.equals(texto, other.texto)
+				&& Objects.equals(titulo, other.titulo);
+	}
+	
+	
+
+}
