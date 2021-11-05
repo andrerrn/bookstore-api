5029b2c9df8cb519bb6f47241a21baa45c9ff244
andrerrn <andre.paz.gomes@gmail.com>
2021-11-04 22:45:25 -0300

    Finalizando as classes de dominio

--- src/main/java/com/andre/bookstore/domain/Categoria.java
+++ src/main/java/com/andre/bookstore/domain/Categoria.java
@@ -1,15 +1,28 @@
 package com.andre.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
-
-public class Categoria {
+@Entity
+public class Categoria  implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
 	
+	@OneToMany(mappedBy =  "categoria")
 	private java.util.List<Livro> livros = new ArrayList<>();
 
 	public Categoria() {
