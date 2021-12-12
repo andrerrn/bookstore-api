ad9fd9b18e51ab4876924f387c7cf556e824f934
andrerrn <andre.paz.gomes@gmail.com>
2021-12-12 18:09:36 -0300

    GET para Livro por ID

---
+++ src/main/java/com/andre/bookstore/service/LivroService.java
@@ -0,0 +1,23 @@
+package com.andre.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andre.bookstore.domain.Livro;
+import com.andre.bookstore.repositories.LivroRepository;
+import com.andre.bookstore.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class LivroService {
+	
+	@Autowired
+	private LivroRepository repository;
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + " Tipo: " + Livro.class.getName()));
+	}
+
+}
