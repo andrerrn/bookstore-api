44cd493626e0db2c744e54834c733dabe5ae3515
andrerrn <andre.paz.gomes@gmail.com>
2021-12-14 21:40:33 -0300

    listando todos os livros de uma categoria

--- src/main/java/com/andre/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/andre/bookstore/repositories/LivroRepository.java
@@ -1,11 +1,19 @@
 package com.andre.bookstore.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.andre.bookstore.domain.Livro;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro , Integer>{
+	
+	
+	@Query("SELECT obj FROM Livro obj WHERE obj.categoria.id =:id_cat ORDER BY titulo")
+	List<Livro> findByAllCategoria(@Param(value = "id_cat" ) Integer id_cat);
 
 }
