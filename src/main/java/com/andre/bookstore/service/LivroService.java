04f092a4ba423e5a8da79e161c4c3f9fb61a6576
andrerrn <andre.paz.gomes@gmail.com>
2021-12-24 19:33:21 -0300

    POST para livros

--- src/main/java/com/andre/bookstore/service/LivroService.java
+++ src/main/java/com/andre/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.andre.bookstore.domain.Categoria;
 import com.andre.bookstore.domain.Livro;
 import com.andre.bookstore.repositories.LivroRepository;
 import com.andre.bookstore.service.exceptions.ObjectNotFoundException;
@@ -38,6 +39,12 @@ public class LivroService {
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
 		
+	}
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 	
 	
