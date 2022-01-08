135d23add2669b31563c24957662f0e3081b973c
andrerrn <andre.paz.gomes@gmail.com>
2022-01-08 16:02:48 -0300

    voltando id para identity

--- src/main/java/com/andre/bookstore/domain/Categoria.java
+++ src/main/java/com/andre/bookstore/domain/Categoria.java
@@ -20,7 +20,7 @@ public class Categoria  implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@NotEmpty(message = "Campo NOME não pode ser vazio")
