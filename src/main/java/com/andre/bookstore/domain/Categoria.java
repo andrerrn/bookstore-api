dfad389f68a2dfaf8f73a3e2d063b14c43595a3d
andrerrn <andre.paz.gomes@gmail.com>
2022-01-08 18:41:17 -0300

    voltadno para identity

--- src/main/java/com/andre/bookstore/domain/Categoria.java
+++ src/main/java/com/andre/bookstore/domain/Categoria.java
@@ -20,7 +20,7 @@ public class Categoria  implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@NotEmpty(message = "Campo NOME não pode ser vazio")
